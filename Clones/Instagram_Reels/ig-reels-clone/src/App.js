62de9480e875acc63bc49a62f0b1d588cb59f1b4
anshulabraham <anshulabraham@gmail.com>
2020-09-08 23:07:10 -0600

    Just starting the project

--- Clones/Instagram_Reels/ig-reels-clone/src/App.js
+++ Clones/Instagram_Reels/ig-reels-clone/src/App.js
@@ -4,8 +4,15 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hey Just starting the cloney</h1>
+    <div className="app">
+    <div className="app_top">
+      <div className="app_videos">
+        <img className="app_logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/1024px-Instagram_logo_2016.svg.png" alt="ig_Image"></img>
+        <h1>Reels</h1>
+      </div>
+
+    </div>
+      
     </div>
   );
 }
